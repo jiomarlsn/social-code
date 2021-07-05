4a0840f82a2da205f151187480afcbd1f717962f
Keven Leone <keven.leone@liferay.com>
2021-07-05 20:58:07 -0300

    Aula 1 - Variáveis e Dados

---
+++ 01-logica-programacao/variavel-e-dados.js
@@ -0,0 +1,28 @@
+// camelCase
+var nomeCompleto = "keven leone";
+
+// snake_case
+let nome_completo = "keven leone";
+
+// Title Case
+const NomeCompleto = "keven leone";
+
+// Tipagem Dinâmica
+
+const idade = 25;
+const casado = true;
+const bornDate = new Date(1996, 1, 19);
+const cidades = ["Palmares", "Recife"]; // Array
+const faculdade = {
+    nome: "POLI",
+    cidade: "Recife",
+    "cidade-sede": "Recife"
+}
+
+let recifeEstaChovendo = undefined;
+
+// Consultar API tempo.com (ex) e verificar se recife está chovendo
+
+// Obtenho a resposta
+
+recifeEstaChovendo = true;
