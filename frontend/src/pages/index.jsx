c20ca0a6ed85ca76020717572fff45ab498903d5
Keven Leone <keven.leone@liferay.com>
2021-08-11 20:17:52 -0300

    Todo App Adicionado

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,46 @@
+import { Button, FormLabel, FormControl } from "react-bootstrap";
+import { useState } from "react";
+
+const Homepage = () => {
+  const [name, setName] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onAddTodo = () => {
+    setTodos([...todos, name]);
+
+    setName("");
+  };
+
+  return (
+    <div className="container mt-4">
+      <h1>SocialCode Lista de Tarefas</h1>
+
+      <div className="mb-3">
+        <FormLabel>O que gostaria de fazer hoje ?</FormLabel>
+        <FormControl
+          name="name"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+          type="text"
+          value={name}
+        />
+        <Button
+          className="mt-2"
+          disabled={name.trim().length <= 3}
+          onClick={() => onAddTodo()}
+        >
+          Adicionar Tarefa
+        </Button>
+      </div>
+
+      <ul>
+        {todos.map((todo, index) => {
+          return <li key={index}> {todo}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Homepage;
