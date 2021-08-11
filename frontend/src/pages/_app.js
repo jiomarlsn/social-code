da78c57c7ba1c20d24688b0624ae65cd0491bc02
Keven Leone <keven.leone@liferay.com>
2021-08-11 20:54:27 -0300

    React Props

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,7 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "../components/header/Header";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Header></Header>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
